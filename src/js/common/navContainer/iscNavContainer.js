3bfb294e01a8791b625d2fbf30f0d062d40d21cf
doug31415 <doug31415@gmail.com>
2015-05-05 10:48:58 -0400

    fix for bug on sessionTimeout

--- src/js/common/navContainer/iscNavContainer.js
+++ src/js/common/navContainer/iscNavContainer.js
@@ -196,18 +196,13 @@
         }
 
         function loadDataFromStoredSession(){
-          //$log.debug( 'ischNavContainer.loadDataFromStoredSession');
+          $log.debug( 'ischNavContainer.loadDataFromStoredSession');
 
-          var timeoutCounter = iscSessionStorageHelper.getSessionTimeoutCounter();
-          //$log.debug( '...got a counter: ' + timeoutCounter );
-          if( timeoutCounter > 0 ){
-            //$log.debug( '...got a counter: ' + timeoutCounter );
-            iscSessionModel.initSessionTimeout( timeoutCounter );
-          }
-
+          // NOTE - set the login response and create the session BEFORE calling initSessionTimeout
+          // since the warning for sessionTimeout time is predicate on setting the sessionTimeout time first
           var storedLoginResponse = iscSessionStorageHelper.getLoginResponse();
           if( !_.isEmpty( storedLoginResponse )){
-            //$log.debug( '...got storedLoginResponse: ' + JSON.stringify( storedLoginResponse ));
+            $log.debug( '...got storedLoginResponse: ' + JSON.stringify( storedLoginResponse ));
             iscSessionModel.create( storedLoginResponse );
 
             var currentUser = iscSessionModel.getCurrentUser();
@@ -216,9 +211,17 @@
 
           var storedStatePermissions = iscSessionStorageHelper.getStoredStatePermissions();
           if( !_.isEmpty( storedStatePermissions )){
-            //$log.debug( '...got permissions: ' + JSON.stringify( storedStatePermissions ));
+            $log.debug( '...got permissions: ' + JSON.stringify( storedStatePermissions ));
             iscSessionModel.setStatePermissions( storedStatePermissions );
           }
+
+          var timeoutCounter = iscSessionStorageHelper.getSessionTimeoutCounter();
+          $log.debug( '...got a counter: ' + timeoutCounter );
+          if( timeoutCounter > 0 ){
+            //$log.debug( '...got a counter: ' + timeoutCounter );
+            iscSessionModel.initSessionTimeout( timeoutCounter );
+          }
+
         }
 
       }]);
