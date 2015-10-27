cd6a10831bee66c592293d20f25196c454c0ccab
Trevor Hudson <trevor.hudson@intersystems.com>
2015-10-27 14:07:55 -0400

    add changes to tests

--- test/unit/common/filters/iscDateFilterTests.js
+++ test/unit/common/filters/iscDateFilterTests.js
@@ -18,7 +18,7 @@
 
     beforeEach( inject( function( $rootScope, $injector ){
       scope = $rootScope.$new();
-      filter = $injector.get('$filter')('iscDateFilter');
+      filter = $injector.get('$filter')('iscDate');
 
       //"2014-12-08 04:57:00" - expected format
     }));
@@ -33,9 +33,9 @@
         month = ( month < 10 ) ? '0' + month : month;
         var day = today.date();
         day = ( day < 10 ) ? '0' + day : day;
-        var dateStr = today.year() + '-' + month + '-' + day + '      04:57:00'; //note extra whitespace
-        var expected  = filter( dateStr );
-        expect( expected ).toBe( 'ISC_TODAY' );
+        var dateStr = today.year() + '-' + month + '-' + day + ' 04:57:00'; //note extra whitespace
+        var expected  = filter( dateStr, 'fromNow' );
+        expect( expected ).toEqual( jasmine.stringMatching('hours ago') );
       });
 
       it( 'should return the right date string - yesterday', function(){
@@ -47,15 +47,33 @@
         day = ( day < 10 ) ? '0' + day : day;
         var dateStr = yesterday.year() + '-' + month + '-' + day + ' 04:57:00';
 
-        var expected  = filter( dateStr );
-        expect( expected ).toBe( 'ISC_YESTERDAY' );
+        var expected  = filter( dateStr, 'fromNow' );
+        expect( expected ).toBe( 'a day ago' );
 
       });
 
       it( 'should return the right date string - some day', function(){
         var dateStr = '2014-12-07 04:57:00';
         var expected  = filter( dateStr );
-        expect( expected ).toBe( '2014-Dec-07' );
+        expect( expected ).toBe( 'December 7th, 2014 4:57 AM' );
+      });
+
+      it( 'should return the right date string - date', function(){
+        var dateStr = '2014-12-07 04:57:00';
+        var expected  = filter( dateStr, 'date' );
+        expect( expected ).toBe( 'December 7th, 2014' );
+      });
+
+      it( 'should return the right date string - dateWithTime', function(){
+        var dateStr = '2014-12-07 04:57:00';
+        var expected = filter( dateStr, 'dateWithTime' );
+        expect( expected ).toBe( '12/7/14 4:57 AM' );
+      });
+
+      it( 'should return the right date string - custom', function(){
+        var dateStr = '2014-12-07 04:57:00';
+        var expected  = filter( dateStr, 'l' );
+        expect( expected ).toBe( '12/7/2014' );
       });
 
     });
