694bb3d1c8dc8092083b7bcb4a35710f4c7fb095
Ryan Jarvis <Ryan.Jarvis@intersystems.com>
2015-11-25 10:41:23 -0500

    Added support for blacklisting channels Added channel prefixing to identify output Added support for trace

--- src/modules/devlog/devlog.js
+++ src/modules/devlog/devlog.js
@@ -10,23 +10,25 @@
   ];
 
   function devlog( $log, iscCustomConfigService ) {
-    var devlog = {};//jshint ignore:line
+    var devlog = {};
 
     // make devlog function as an extension of angular $log
     // bruteforce approach
-    devlog.log = function() { $log.log.apply(this, arguments); };
-    devlog.info = function() { $log.info.apply(this, arguments); };
-    devlog.warn = function() { $log.warn.apply(this, arguments); };
-    devlog.error = function() { $log.error.apply(this, arguments); };
-    devlog.debug = function() { $log.debug.apply(this, arguments); };
+    devlog.log = function() { var args = devlog._prefix_args(arguments); $log.log.apply(this, args); devlog._clear_channel_prefix(); };
+    devlog.info = function() { var args = devlog._prefix_args(arguments); $log.info.apply(this, args); devlog._clear_channel_prefix(); };
+    devlog.warn = function() { var args = devlog._prefix_args(arguments); $log.warn.apply(this, args); devlog._clear_channel_prefix(); };
+    devlog.error = function() { var args = devlog._prefix_args(arguments); $log.error.apply(this, args); devlog._clear_channel_prefix(); };
+    devlog.debug = function() { var args = devlog._prefix_args(arguments); $log.debug.apply(this, args); devlog._clear_channel_prefix(); };
+    devlog.trace = function() { console.trace(); };
 
     // null interface for channels that fail 
-    var nullobj = {};
+    var nullobj = {}
     nullobj.log = function() {};
     nullobj.info = function() {};
     nullobj.warn = function() {};
     nullobj.error = function() {};
     nullobj.debug = function() {};
+    nullobj.trace = function() {};
 
     //channel acts as a filter for log messages by
     //either passing them directly to $log interface if whitelisted
@@ -44,7 +46,7 @@
 
       var config = iscCustomConfigService.getConfig();
       if(!config) {
-        $log.debug('WARNING No config in application, suppressing call to devlog');
+        $log.debug("WARNING No config in application, suppressing call to devlog");
         console.trace();
         return nullobj;
       }
@@ -57,21 +59,50 @@
       }
 
       //no channel specified
-      //or wildcard in whitelist
-      //means send it directly through
-      if (!args.length || (whitelist.indexOf('*') >= 0)) {
+      //means send it directly through without prefixing
+      if (!args.length) {
         return devlog;
       }
 
-      //if any channel specified appears in whitelist, send it through
-      for(var ii = 0; ii < args.length; ii++) {
-        if( whitelist.indexOf( args[ii] ) >= 0 ) {
-          return devlog;
+
+      //whittle channels down to approved ones
+      //if any, send through with prefixing
+      //otherwise suppress
+      var approved = [];
+      for(var i = 0; i < args.length; i++) {
+        if ( whitelist.indexOf( args[i] ) >= 0 ) {
+          approved.push(args[i]);
+        } else if( whitelist.indexOf("*") >= 0 && whitelist.indexOf("!" + args[i]) < 0) {
+          approved.push(args[i]);
         }
+      }
+      if(approved.length > 0) {
+        devlog._set_channel_prefix(approved);
+        return devlog;
       }
 
       return nullobj;
       
+    };
+
+    devlog._channel_prefix = "";
+
+    devlog._set_channel_prefix = function(chans) {
+      devlog._channel_prefix = "|" + chans.join('|') + "| ";
+      devlog._channel_prefix = devlog._channel_prefix.toUpperCase();
+    };
+    devlog._clear_channel_prefix = function() {
+      devlog._channel_prefix = "";
+    };
+    devlog._prefix_args = function(msg_params) {
+      //msg_params is of type Arguments from function
+      var args = Array.prototype.slice.call(msg_params);
+      if (args[0] && (typeof args[0] === 'string' || args[0] instanceof String)) {
+        args[0] = devlog._channel_prefix + args[0]; 
+      } else {
+        args.unshift(devlog._channel_prefix);
+      }
+      return args;
     };
 
     return devlog;
--- test/unit/common/devlog/devlogTests.js
+++ test/unit/common/devlog/devlogTests.js
@@ -16,129 +16,128 @@
       iscCustomConfigService = _iscCustomConfigService_;
 
       mockConfig = angular.copy( customConfig );
-      spyOn( iscCustomConfigService, 'getConfig').and.returnValue( mockConfig );
+      spyOn( iscCustomConfigService, 'getConfig').andReturn( mockConfig );
 
     }));
 
     describe(
-        'basic log calls',
-        function() {
-          it(
-              'should function as a wrapper to $log - string',
-              function() {
-
-                devlog.log('This is a log message');
-                //console.log( '$log', $log.log );
-                expect( $log.log.logs[0][0] ).toBe( 'This is a log message' );
-              }
-          );
-          it(
-              'should function as a wrapper to $log - object',
-              function() {
-                devlog.log({a:1});
-                expect( $log.log.logs[0][0] ).toEqual( {a:1} );
-              }
-          );
-        }
+      'basic debug calls',
+      function() {
+        it(
+          'should function as a wrapper to $log - string',
+          function() {
+            //console.log( '$log', $log );
+            devlog.debug('This is a debug message');
+            expect( $log.debug.logs[0][0] ).toBe( 'This is a debug message' );
+          }
+        );
+        it(
+          'should function as a wrapper to $log - object',
+          function() {
+            devlog.debug({a:1});
+            expect( $log.debug.logs[0][1] ).toEqual( {a:1} );
+          }
+        );
+      }
     );
 
     describe(
-        'channel usage with no channels',
-        function() {
-          it(
-              'should not go through - no whitelist present',
-              function() {
-                mockConfig.devlogWhitelist = undefined;
-                devlog.channel().log('This is a log message');
-                expect( $log.log.logs ).toEqual( [] );
-              }
-          );
-          it(
-              'should always go through - no whitelist entries',
-              function() {
-                mockConfig.devlogWhitelist = [];
-                devlog.channel().log('This is a log message');
-                expect( $log.log.logs[0][0] ).toBe( 'This is a log message' );
-              }
-          );
-          it(
-              'should always go through - with whitelist entries',
-              function() {
-                mockConfig.devlogWhitelist = [];
-                mockConfig.devlogWhitelist.push('apple');
-                mockConfig.devlogWhitelist.push('banana');
-                devlog.channel().log('This is a log message');
-                expect( $log.log.logs[0][0] ).toBe( 'This is a log message' );
-              }
-          );
-        }
+      'channel usage with no channels',
+      function() {
+        it(
+          'should not go through - no whitelist present',
+          function() {
+            mockConfig.devlogWhitelist = undefined;
+            devlog.channel().debug('This is a debug message');
+            expect( $log.debug.logs ).toEqual( [] );
+          }
+        );
+        it(
+          'should always go through - no whitelist entries',
+          function() {
+            mockConfig.devlogWhitelist = [];
+            devlog.channel().debug('This is a debug message');
+            expect( $log.debug.logs[0][0] ).toBe( 'This is a debug message' );
+          }
+        );
+        it(
+          'should always go through - with whitelist entries',
+          function() {
+            mockConfig.devlogWhitelist = [];
+            mockConfig.devlogWhitelist.push('apple');
+            mockConfig.devlogWhitelist.push('banana');
+            devlog.channel().debug('This is a debug message');
+            expect( $log.debug.logs[0][0] ).toBe( 'This is a debug message' );
+          }
+        );
+      }
     );
 
     describe(
-        'channel usage with one channel',
-        function() {
-          it(
-              'should not go through with no whitelist present',
-              function() {
-                mockConfig.devlogWhitelist = undefined;
-                devlog.channel('banana').log('This is a log message');
-                expect( $log.log.logs ).toEqual( [] );
-              }
-          );
-          it(
-              'should go through when whitelisted',
-              function() {
-                mockConfig.devlogWhitelist = [];
-                mockConfig.devlogWhitelist.push('apple');
-                mockConfig.devlogWhitelist.push('banana');
-                devlog.channel('banana').log('This is a log message');
-                expect( $log.log.logs[0][0] ).toBe( 'This is a log message' );
-              }
-          );
-          it(
-              'should not go through when not whitelisted',
-              function() {
-                mockConfig.devlogWhitelist = [];
-                mockConfig.devlogWhitelist.push('apple');
-                mockConfig.devlogWhitelist.push('banana');
-                devlog.channel('cookie').log('This is a log message');
-                expect( $log.log.logs ).toEqual( [] );
-              }
-          );
-        }
+      'channel usage with one channel',
+      function() {
+        it(
+          'should not go through with no whitelist present',
+          function() {
+            mockConfig.devlogWhitelist = undefined;
+            devlog.channel('banana').debug('This is a debug message');
+            expect( $log.debug.logs ).toEqual( [] );
+          }
+        );
+        it(
+          'should go through when whitelisted',
+          function() {
+            mockConfig.devlogWhitelist = [];
+            mockConfig.devlogWhitelist.push('apple');
+            mockConfig.devlogWhitelist.push('banana');
+            devlog.channel('banana').debug('This is a debug message');
+            expect( $log.debug.logs[0][0] ).toBe( '|BANANA| This is a debug message' );
+          }
+        );
+        it(
+          'should not go through when not whitelisted',
+          function() {
+            mockConfig.devlogWhitelist = [];
+            mockConfig.devlogWhitelist.push('apple');
+            mockConfig.devlogWhitelist.push('banana');
+            devlog.channel('cookie').debug('This is a debug message');
+            expect( $log.debug.logs ).toEqual( [] );
+          }
+        );
+      }
     );
 
     describe(
-        'channel usage with multiple channels',
-        function() {
-          it(
-              'should not go through with no whitelist present',
-              function() {
-                mockConfig.devlogWhitelist = undefined;
-                devlog.channel('cookie', 'banana', 'duck').log('This is a log message');
-                expect( $log.log.logs ).toEqual( [] );
-              }
-          );
-          it(
-              'should go through when any are whitelisted',
-              function() {
-                //console.log( mockConfig );
-                mockConfig.devlogWhitelist.push('apple');
-                mockConfig.devlogWhitelist.push('banana');
-                devlog.channel('cookie', 'banana', 'duck').log('This is a log message');
-                expect( $log.log.logs[0][0] ).toBe( 'This is a log message' );
-              }
-          );
-          it(
-              'should not go through when none are whitelisted',
-              function() {
-                mockConfig.devlogWhitelist.push('apple');
-                mockConfig.devlogWhitelist.push('banana');
-                devlog.channel('cookie', 'duck', 'eagle').log('This is a log message');
-                expect( $log.log.logs ).toEqual( [] );
-              }
-          );
-        }
+      'channel usage with multiple channels',
+      function() {
+        it(
+          'should not go through with no whitelist present',
+          function() {
+            mockConfig.devlogWhitelist = undefined;
+            devlog.channel('cookie', 'banana', 'duck').debug('This is a debug message');
+            expect( $log.debug.logs ).toEqual( [] );
+          }
+        );
+        it(
+          'should go through when any are whitelisted',
+          function() {
+            //console.log( mockConfig );
+            mockConfig.devlogWhitelist.push('apple');
+            mockConfig.devlogWhitelist.push('banana');
+            devlog.channel('cookie', 'banana', 'duck').debug('This is a debug message');
+            expect( $log.debug.logs[0][0] ).toBe( '|BANANA| This is a debug message' );
+          }
+        );
+        it(
+          'should not go through when none are whitelisted',
+          function() {
+            mockConfig.devlogWhitelist.push('apple');
+            mockConfig.devlogWhitelist.push('banana');
+            devlog.channel('cookie', 'duck', 'eagle').debug('This is a debug message');
+            expect( $log.debug.logs ).toEqual( [] );
+          }
+        );
+      }
     );
   });
 
