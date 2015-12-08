dea780be4413f8642f590bb84c0de7313642b660
Henry Zou <henry.zou@intersystems.com>
2015-12-08 09:17:29 -0500

    moving code under common folder

---
+++ src/common/modules/isc.common.js
@@ -0,0 +1,30 @@
+/**
+ * Created by douglasgoodman on 11/21/14.
+ */
+
+(function () {
+  'use strict';
+
+  angular.module('isc.common',
+    [
+      // third party modules
+      'angularUtils.directives.dirPagination',
+      'ui.router',
+      'mobile-angular-ui',
+      'mobile-angular-ui.gestures',
+      'ngSanitize',
+      'ngAnimate',
+      'pascalprecht.translate',
+      'angular.filter',
+      '720kb.datepicker',
+      'foundation',
+      'angucomplete-alt',
+
+      // logging helper
+      //'logLineNumber',
+
+      // isc tabbed navigation module
+      'iscNavContainer'
+    ]);
+
+})();
