015559310d33dc3abccebec0f51227eadb487572
Henry Zou <henry.zou@intersystems.com>
2015-12-25 16:38:31 -0500

    adding files for isc state module and run

---
+++ src/common/modules/states/iscStateManager.module.js
@@ -0,0 +1,16 @@
+/**
+ * Created by douglas goodman on 3/9/15.
+ */
+
+(function () {
+  'use strict';
+
+  // ----------------------------
+  // injection
+  // ----------------------------
+
+  angular.module('isc.states', []);
+
+
+})();
+
