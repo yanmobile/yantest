8e280db9f7b370fd1fce84c25bcb59af72fc747d
Henry Zou <henry.zou@intersystems.com>
2016-01-15 13:28:54 -0500

    socket

---
+++ src/common/modules/socket/socket.module.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hzou on 12/28/15.
+ */
+
+//SRC LINK: http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
+
+(function () {
+  'use strict';
+
+  angular
+    .module('isc.socket', []);
+
+})();
