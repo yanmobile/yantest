aafc5258403120e58c257ed946a96e6a216d7336
Henry Zou <henry.zou@intersystems.com>
2016-01-15 13:14:46 -0500

    adding socket to core

---
+++ src/common/modules/socket/socket.js
@@ -0,0 +1,44 @@
+/**
+ * Created by hzou on 12/28/15.
+ */
+
+//SRC LINK: http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
+
+(function () {
+  'use strict';
+
+  angular
+    .module('isc.socket')
+    .factory('socket', socket);
+
+  function socket(devlog, $rootScope) {
+    devlog.channel('socket').debug('socket LOADED');
+    var socket = io.connect();
+    return {
+      on  : on,
+      emit: emit
+    };
+
+    function on(eventName, callback) {
+      devlog.channel('socket').debug('on called');
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    }
+
+    function emit(eventName, data, callback) {
+      devlog.channel('socket').debug('emit called');
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  }
+})();
