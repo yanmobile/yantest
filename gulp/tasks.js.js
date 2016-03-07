5c7558c272f350eade709152f0ce17293b1cf9db
Henry Zou <hzou@intersystems.com>
2016-03-07 17:11:03 -0500

    removing unnecessary ngFilesort() from gulp task.js.js

--- gulp/tasks.js.js
+++ gulp/tasks.js.js
@@ -23,21 +23,25 @@ function init(gulp, plugins, config) {
       strip : 'app/modules/'
     };
 
+    var jsSrc = plugins._.concat(
+      config.common.vendor.js,
+      config.component.vendor.js,
+      config.app.vendor.js,
+      plugins._.get(config.common, 'module.assets.vendor.js', []),
+      plugins._.get(config.component, 'module.assets.vendor.js', []),
+      plugins._.get(config.app, 'module.assets.vendor.js', []),
+      config.common.module.js,
+      config.component.module.js,
+      config.app.module.js);
+
     plugins.streamqueue({ objectMode: true },
-      gulp.src(config.common.vendor.js),
-      gulp.src(config.app.vendor.js),
-      gulp.src(plugins._.get(config.common, 'module.assets.vendor.js', [])),
-      gulp.src(plugins._.get(config.app, 'module.assets.vendor.js', [])),
-      gulp.src(config.app.module.js),
-
-      gulp.src(config.common.module.js).pipe(plugins.ngFilesort()),
+      gulp.src(jsSrc),
       gulp.src(config.common.module.html).pipe(plugins.templateCache(commonOpts)),
-
-      gulp.src(config.app.module.js).pipe(plugins.ngFilesort()),
       gulp.src(config.app.module.html).pipe(plugins.templateCache(customOpts))
     )
+      .pipe(plugins.filelog())
       .pipe(plugins.ngAnnotate())
-      //.pipe(uglify())
+      //.pipe(plugins.uglify())
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.concat('app.js'))
       .pipe(plugins.rename({ suffix: '.min' }))
