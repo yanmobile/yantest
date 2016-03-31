b3683bf73cf5998a43e27071948128edcde2e0af
probbins <paul.robbins@intersystems.com>
2016-03-31 15:24:50 -0400

    Revert "Moves moduleApi from appConfig to componentsConfig"
    This reverts commit b7efdf8c2c03a7e1e8ccc0ec8a74289104a0ac41.

--- src/components/foundation/base/modules/isc.components.config.js
+++ src/components/foundation/base/modules/isc.components.config.js
@@ -18,23 +18,6 @@
       'session'        : {
         'routeMemoryExpirationInMinutes': 15
       },
-      'moduleApi' : {
-        'forms' : {
-          'path'    : 'api/v1/forms'
-        },
-        'formData' : {
-          'path'    : 'api/v1/formData'
-        },
-        'formInfo' : {
-          'path'    : 'api/v1/formInfo'
-        },
-        'formTemplates' : {
-          'path'    : 'api/v1/formTemplates'
-        },
-        'formCodeTables' : {
-          'path'    : 'api/v1/codeTables'
-        }
-      },
       'debugDisplay'   : {
         'forms': {
           'additionalModels': false,
