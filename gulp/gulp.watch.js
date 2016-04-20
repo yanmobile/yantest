3de0dbc2b81b44d86334653c98de04dca2c58fb3
Henry Zou <hzou@intersystems.com>
2016-04-20 10:15:59 -0400

    fixing issue with gulp-watch not monitoring new/deleted files

--- gulp/gulp.watch.js
+++ gulp/gulp.watch.js
@@ -1,38 +1,47 @@
-
 module.exports = {
   init: init
 };
 
+var watch = require('gulp-watch');
+
 function init(gulp, plugins, config, _) {
 
-  gulp.task('watch', [] ,function () {
+  var watchlist = {
+      'src/common/assets/images/**/*'        : ['images'],
+      'src/common/assets/sass/**/*'          : ['sass'],
+      'src/common/assets/vendors/**/*'       : ['scripts'],
+      'src/common/modules/**/*.js'           : ['test:common', 'jshint:common', 'scripts:common'],
+      'src/common/modules/**/*.html'         : ['templates'],
+      'test/unit/common/**/*.js'             : ['test:common'],
+    
+      'src/components/**/assets/images/**/*' : ['images'],
+      'src/components/**/assets/sass/**/*'   : ['sass'],
+      'src/components/**/assets/vendors/**/*': ['scripts'],
+      'src/components/**/modules/**/*.js'    : ['test:components', 'jshint:components', 'scripts:components'],
+      'src/components/**/modules/**/*.html'  : ['templates'],
+      'test/unit/components/**/*.js'         : ['test:components'],
+    
+      'src/app/assets/i18n/**/*'             : ['i18n'],
+      'src/app/assets/images/**/*'           : ['images'],
+      'src/app/assets/sass/**/*'             : ['sass'],
+      'src/app/assets/mockData/**/*'         : ['mocks', 'scripts'],
+      'src/app/assets/vendors/**/*'          : ['scripts'],
+      'src/app/modules/**/*.js'              : ['test:app', 'jshint:app', 'scripts:app'],
+      'src/app/modules/**/*.html'            : ['templates'],
+      'test/unit/app/**/*.js'                : ['test:app'],
+      'src/index.html'                       : ['html'],
+      'gulp/**'                              : ['build']
+    };
 
-    gulp.watch('src/common/assets/images/**/*',           ['images']);
-    gulp.watch('src/common/assets/sass/**/*',             ['sass']);
-    gulp.watch('src/common/assets/vendors/**/*',          ['scripts']);
-    gulp.watch('src/common/modules/**/*.js',              ['test:common', 'jshint', 'scripts']);
-    gulp.watch('src/common/modules/**/*.html',            ['templates']);
-    gulp.watch('test/unit/common/**/*.js',                ['test:common']);
 
-    gulp.watch('src/components/**/assets/images/**/*',    ['images']);
-    gulp.watch('src/components/**/assets/sass/**/*',      ['sass']);
-    gulp.watch('src/components/**/assets/vendors/**/*',   ['scripts']);
-    gulp.watch('src/components/**/modules/**/*.js',       ['test:components', 'jshint', 'scripts']);
-    gulp.watch('src/components/**/modules/**/*.html',     ['templates']);
-    gulp.watch('test/unit/components/**/*.js',            ['test:components']);
+  gulp.task('watch', function () {
+    _.forEach(watchlist, watchItem);
 
-    gulp.watch('src/app/assets/i18n/**/*',                ['i18n']);
-    gulp.watch('src/app/assets/images/**/*',              ['images']);
-    gulp.watch('src/app/assets/sass/**/*',                ['sass']);
-    gulp.watch('src/app/assets/mockData/**/*',            ['mocks', 'scripts']);
-    gulp.watch('src/app/assets/vendors/**/*',             ['scripts']);
-    gulp.watch('src/app/modules/**/*.js',                 ['test:app', 'jshint', 'scripts']);
-    gulp.watch('src/app/modules/**/*.html',               ['templates']);
-    gulp.watch('test/unit/app/**/*.js',                   ['test:app']);
-
-    gulp.watch('src/index.html',                          ['html']);
-
-    gulp.watch('gulp/**',                                 ['build']);
+    function watchItem(tasks, src) {
+      return watch(src, function () {
+        return gulp.start(tasks);
+      });
+    }
   });
 
 }
