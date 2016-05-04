c698e217bb01f8d85fee50624948a583724c5c68
Henry Zou <hzou@intersystems.com>
2016-05-04 08:52:54 -0400

    disabling debugInfo when in production. see: https://docs.angularjs.org/guide/production

--- src/app/modules/app.config.js
+++ src/app/modules/app.config.js
@@ -14,6 +14,7 @@
    ========================================*/
   function getConfig() {
     return {
+      'production': false,
       'api': {
         'protocol': 'http',
         'hostname': 'localhost',
