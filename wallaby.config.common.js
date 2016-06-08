6b7159158ba428410349afdc9f16dba0bfe51c57
Henry Zou <hzou@intersystems.com>
2016-06-08 18:59:31 -0400

    HSFW-84 - updating wallabyjs config to use src files from common and components without duplication

--- wallaby.config.common.js
+++ wallaby.config.common.js
@@ -1,44 +1,69 @@
-var angularTemplatePreprocessor = require('wallaby-ng-html2js-preprocessor');
-module.exports                  = function () {
+var angularTemplatePreprocessor = require( 'wallaby-ng-html2js-preprocessor' );
+
+
+module.exports = wallaby;
+
+wallaby.getSrc       = getSrc;
+wallaby.noInstrument = noInstrument;
+function wallaby() {
   'use strict';
+  var src = getSrc();
 
-  var commonConfig = require('./gulp/common.json');
-
-  var commonVendorJs      = (commonConfig.vendor.js || []).map(noInstrument);
-  var commonModuleVendorJs = (commonConfig.module.assets.vendor.js || []).map(noInstrument);
-  var commonVendorMocks   = (commonConfig.vendor.mocks || []).map(noInstrument);
-  var commonModuleMocks   = (commonConfig.module.mocks || []).map(noInstrument);
-  var commonModuleModules = commonConfig.module.modules || [];
-  var commonModuleJs      = commonConfig.module.js || [];
-  var commonModuleTests   = commonConfig.module.tests || [];
-  var commonModuleHtml    = commonConfig.module.html || [];
   return {
     'files'        : []
-      .concat(commonVendorJs)
-      .concat(commonModuleVendorJs)
-      .concat(commonVendorMocks)
-      .concat(commonModuleMocks)
-      .concat(commonModuleModules)
-      .concat(commonModuleJs)
-      .concat(commonModuleHtml),
-    'tests'        : commonModuleTests,
+      .concat( src.commonVendorJs )
+      .concat( src.commonModuleVendorJs )
+      .concat( src.commonVendorMocks )
+      .concat( src.commonModuleMocks )
+      .concat( src.commonModuleModules )
+      .concat( src.commonModuleJs )
+      .concat( src.commonModuleHtml ),
+    'tests'        : src.commonModuleTests,
     'preprocessors': {
-      'src/common/modules/**/*.html': function (file) {
-        return angularTemplatePreprocessor.transform(file, {
-          stripPrefix: 'src\/(app|common|components)\/.*\/?modules\/',
-          'moduleName' : 'isc.templates'
-        });
+      'src/common/modules/**/*.html': function( file ) {
+        return angularTemplatePreprocessor.transform( file, {
+          stripPrefix : 'src\/(app|common|components)\/.*\/?modules\/',
+          'moduleName': 'isc.templates'
+        } );
       }
     },
     'testFramework': 'jasmine'
   };
 
-  /**
-   * @description Used by WallabyJs to not run code analysis
-   * @param pattern
-   * @returns {{pattern: *, instrument: boolean}}
-   */
-  function noInstrument(pattern) {
-    return { pattern: pattern, instrument: false };
+}
+
+function getSrc() {
+  var commonConfig = require( './gulp/common.json' );
+  // var karmaTestConfig = require( './test/karma.conf.common' );
+  // var src             = karmaTestConfig.getSrc( { common: commonConfig } );
+
+  return {
+    commonVendorJs      : (commonConfig.vendor.js || []).map( noInstrument ),
+    commonModuleVendorJs: (commonConfig.module.assets.vendor.js || []).map( noInstrument ),
+    commonVendorMocks   : (commonConfig.vendor.mocks || []).map( noInstrument ),
+    commonModuleMocks   : (commonConfig.module.mocks || []).map( noInstrument ),
+    commonModuleModules : commonConfig.module.modules || [],
+    commonModuleJs      : commonConfig.module.js || [],
+    commonModuleTests   : commonConfig.module.tests || [],
+    commonModuleHtml    : commonConfig.module.html || []
+  };
+}
+
+/**
+ * @description Used by WallabyJs to not run code analysis
+ * @param pattern
+ * @returns {{pattern: *, instrument: boolean}}
+ */
+function noInstrument(pattern) {
+  if (_.isObject(pattern)) {
+    return _.merge(pattern, {
+      instrument: false
+    });
   }
-};
+  else {
+    return {
+      pattern   : pattern,
+      instrument: false
+    };
+  }
+}
