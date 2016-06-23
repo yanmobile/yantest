4fc140c64691da29609d95f6d306ba50da7cc87d
probbins <paul.robbins@intersystems.com>
2016-06-23 11:00:01 -0400

    Added sample collection data and editing test

--- test/unit/components/forms/widgets/iscSubform.directive.test.js
+++ test/unit/components/forms/widgets/iscSubform.directive.test.js
@@ -10,7 +10,7 @@
     beforeEach( module(
       'formly', 'foundation',
       'isc.http', 'isc.forms', 'iscNavContainer', 'isc.authorization', 'isc.notification', 'isc.directives',
-      'isc.templates',
+      'isc.templates', 'isc.fauxTable', 'isc.filters',
       function( $provide, devlogProvider ) {
         $provide.value( '$log', console );
         $provide.value( 'apiHelper', mockApiHelper );
@@ -37,9 +37,210 @@
         validationService  : iscFormsValidationService
       } );
       mockFormResponses( suiteMain.$httpBackend );
+    } ) );
 
+    describe( 'iscSubform - new', function() {
+      beforeEach( function() {
+        createDirectives( getConfiguredForm() );
+      } );
+
+      //--------------------
+      it( 'should close a subform when cancel is clicked', function() {
+        // Open a subform of each editAs type, enter a field, and click cancel
+        test( 'test.SubformPage', true );
+        test( 'test.SubformInline' );
+        test( 'test.SubformModal' );
+
+        function test( subformName, isFullPage ) {
+          var suite        = suiteSubform,
+              subform      = getControlByName( suite, subformName ).filter( '.subform' ),
+              addButton    = subform.find( 'button.embedded-form-add' ),
+              model        = _.get( suite.controller.model, subformName ),
+              cancelButton = null,
+              shownForm    = null,
+              inputField   = null;
+
+          expect( addButton.length ).toBe( 1 );
+          expect( subform.length ).toBe( 1 );
+          expect( model ).toBeUndefined();
+
+          // Open the subform
+          addButton.click();
+          digest( suite );
+
+          selectElements();
+
+          // Verify it opened, change a field
+          if ( isFullPage ) {
+            spyOn( suite.controller.childConfig, 'onCancel' ).and.callThrough();
+          }
+          else {
+            expect( shownForm.length ).toBe( 1 );
+          }
+          expect( cancelButton.length ).toBe( 1 );
+          expect( inputField.length ).toBe( 1 );
+
+          // Change a field and click cancel to close it
+          inputField.val( 'some value' ).trigger( 'change' );
+          cancelButton.click();
+          digest( suite );
+          selectElements();
+
+          expect( cancelButton.length ).toBe( 0 );
+          expect( inputField.length ).toBe( 0 );
+          expect( model ).toBeUndefined();
+
+          if ( isFullPage ) {
+            expect( suite.controller.childConfig.onCancel ).toHaveBeenCalled();
+          }
+          else {
+            expect( shownForm.length ).toBe( 0 );
+          }
+
+          function selectElements() {
+            cancelButton = suite.element.find( '.embedded-form-cancel' );
+            shownForm    = subform.find( '.formly' );
+            inputField   = getControlByName( suite, 'aField' );
+          }
+        }
+      } );
+    } );
+
+
+    //--------------------
+    describe( 'iscSubform - edit', function() {
+      beforeEach( function() {
+        createDirectives( getFormWithData() );
+      } );
+
+      //--------------------
+      it( 'should allow entering of data in a subform', function() {
+        // Open a subform of each editAs type, enter a field, save, then edit
+        test( 'test.SubformPage', true );
+        test( 'test.SubformInline' );
+        test( 'test.SubformModal' );
+
+        function test( subformName, isFullPage ) {
+          var suite      = suiteSubform,
+              subform    = getControlByName( suite, subformName ).filter( '.subform' ),
+              addButton  = subform.find( 'button.embedded-form-add' ),
+              model      = _.get( suite.controller.model, subformName ),
+              saveButton = null,
+              shownForm  = null,
+              inputField = null;
+
+          expect( addButton.length ).toBe( 1 );
+          expect( subform.length ).toBe( 1 );
+          expect( model.length ).toBe( 1 );
+
+          // Open the subform
+          addButton.click();
+          digest( suite );
+          selectElements();
+
+          // Verify it opened, change a field
+          if ( isFullPage ) {
+            spyOn( suite.controller.childConfig, 'onSubmit' ).and.callThrough();
+          }
+          else {
+            expect( shownForm.length ).toBe( 1 );
+          }
+          expect( saveButton.length ).toBe( 1 );
+          expect( inputField.length ).toBe( 1 );
+
+          // Change a field and click submit to save it
+          inputField.val( 'some value' ).trigger( 'change' );
+          saveButton.click();
+          digest( suite );
+          selectElements();
+
+          expect( saveButton.length ).toBe( 0 );
+          expect( inputField.length ).toBe( 0 );
+
+          if ( isFullPage ) {
+            expect( suite.controller.childConfig.onSubmit ).toHaveBeenCalled();
+          }
+          else {
+            expect( shownForm.length ).toBe( 0 );
+          }
+          expect( model.length ).toBe( 2 );
+
+          function selectElements() {
+            saveButton = suite.element.find( '.embedded-form-save' );
+            shownForm  = subform.find( '.formly' );
+            inputField = getControlByName( suite, 'aField' );
+          }
+        }
+      } );
+
+      // //--------------------
+      // it( 'should allow editing of data in a subform', function() {
+      //   // Open a subform of each editAs type, enter a field, save, then edit
+      //   // test( 'test.SubformPage', true );
+      //   test( 'test.SubformInline' );
+      //   test( 'test.SubformModal' );
+      //
+      //   function test( subformName, isFullPage ) {
+      //     // suiteMain.$rootScope.$digest();
+      //     var suite      = suiteSubform,
+      //         subform    = getControlByName( suite, subformName ).filter( '.subform' ),
+      //         editButton = subform.find( 'button.embedded-form-edit' ),
+      //         model      = _.get( suite.controller.model, subformName ),
+      //         saveButton = null,
+      //         shownForm  = null,
+      //         inputField = null;
+      //
+      //     expect( subform.length ).toBe( 1 );
+      //     expect( model.length ).toBe( 1 );
+      //     expect( editButton.length ).toBe( 1 );
+      //
+      //     // Open the subform
+      //     editButton.click();
+      //     suite.$scope.$digest();
+      //     // digest( suite );
+      //     selectElements();
+      //
+      //     // Verify it opened, change a field
+      //     if ( isFullPage ) {
+      //       spyOn( suite.controller.childConfig, 'onSubmit' ).and.callThrough();
+      //     }
+      //     else {
+      //       expect( shownForm.length ).toBe( 1 );
+      //     }
+      //     expect( saveButton.length ).toBe( 1 );
+      //     expect( inputField.length ).toBe( 1 );
+      //
+      //     // Change a field and click submit to save it
+      //     inputField.val( 'some different value' ).trigger( 'change' );
+      //     saveButton.click();
+      //     digest( suite );
+      //     selectElements();
+      //
+      //     expect( saveButton.length ).toBe( 0 );
+      //     expect( inputField.length ).toBe( 0 );
+      //
+      //     if ( isFullPage ) {
+      //       expect( suite.controller.childConfig.onSubmit ).toHaveBeenCalled();
+      //     }
+      //     else {
+      //       expect( shownForm.length ).toBe( 0 );
+      //     }
+      //     expect( model.length ).toBe( 1 );
+      //
+      //     function selectElements() {
+      //       saveButton = suite.element.find( '.embedded-form-save' );
+      //       shownForm  = subform.find( '.formly' );
+      //       inputField = getControlByName( suite, 'aField' );
+      //     }
+      //   }
+      //
+      // } );
+
+    } );
+
+    function createDirectives( rootForm ) {
       // Create an isc-form to get what would normally be passed to isc-form-internal
-      suiteForm = createDirective( getConfiguredForm(), {
+      suiteForm = createDirective( rootForm, {
         localFormConfig  : {},
         localButtonConfig: {}
       } );
@@ -56,65 +257,7 @@
       } );
 
       suiteSubform.controller = suiteSubform.$isolateScope.subformCtrl;
-    } ) );
+    }
 
-    //--------------------
-    it( 'should close a subform when cancel is clicked - Modal and Inline', function() {
-      // Open a subform of each editAs type, enter a field, and click cancel
-      test( 'testSubformPage', true );
-      test( 'testSubformInline' );
-      test( 'testSubformModal' );
-
-      function test( subformName, isFullPage ) {
-        var suite     = suiteSubform,
-            subform   = getControlByName( suite, subformName ).filter( '.subform' ),
-            addButton = subform.find( 'button.embedded-form-add' );
-
-        expect( addButton.length ).toBe( 1 );
-        expect( subform.length ).toBe( 1 );
-
-        // Open the subform
-        addButton.click();
-        digest( suite );
-
-        var cancelButton = suite.element.find( '.embedded-form-cancel' ),
-            shownForm    = subform.find( '.formly' ),
-            inputField   = getControlByName( suite, 'aField' );
-
-        // Verify it opened, change a field
-        if ( isFullPage ) {
-          spyOn( suite.controller.childConfig, 'onCancel' ).and.callThrough();
-        }
-        else {
-          expect( shownForm.length ).toBe( 1 );
-        }
-        expect( cancelButton.length ).toBe( 1 );
-        expect( inputField.length ).toBe( 1 );
-
-        inputField.val( 'some value' ).trigger( 'change' );
-        cancelButton.click();
-        digest( suite );
-
-        // Click cancel to close it
-        cancelButton = suite.element.find( '.embedded-form-cancel' );
-        shownForm    = subform.find( '.formly' );
-        inputField   = getControlByName( suite, 'aField' );
-
-        expect( cancelButton.length ).toBe( 0 );
-        expect( inputField.length ).toBe( 0 );
-
-        if ( isFullPage ) {
-          expect( suite.controller.childConfig.onCancel ).toHaveBeenCalled();
-        }
-        else {
-          expect( shownForm.length ).toBe( 0 );
-        }
-      }
-    } );
-
-    //--------------------
-    it( 'should allow editing of data in a subform', function() {
-      // TODO
-    } );
   } );
 })();
