4689cda50d011751b72aec698fca230330db5535
hiiq <highiq@email.com>
2016-09-29 13:54:26 -0400

    Latest updates to lookahead component

--- src/components/foundation/base/modules/isc.lookahead/isc.lookahead.component.js
+++ src/components/foundation/base/modules/isc.lookahead/isc.lookahead.component.js
@@ -5,12 +5,16 @@
     .module( 'isc.lookahead' )
     .component( 'iscLookaheadComponent', {
       bindings    : {
-        formConfig: '=',
-        templates : '=',
         tags      : '=',
         source    : '=',
+        rule      : '<',
+        config    : '<',
         onAdd     : '&',
-        onRemove  : '&'
+        onAdding  : '&',
+        onRemove  : '&',
+        onRemoving: '&',
+        onInvalid : '&',
+        onClicked : '&'
       },
       controller  : lookaheadController,
       controllerAs: 'lookCtrl',
@@ -19,47 +23,145 @@
       }]
     } );
 
+  /**
+   * @ngdoc component
+   * @memberOf isc.lookahead
+   * @description Tag styled input text box with auto-complete functionality
+   * @param devlog
+   * @param lookaheadConfiguration
+   */
   /* @ngInject */
-  function lookaheadController( devlog, $scope, $http ) {
+  function lookaheadController( devlog, lookaheadConfiguration ) {
     var log = devlog.channel( 'iscLookaheadComponent' );
     log.info( 'isc.lookahead.js LOADED' );
 
-    var self = this;
+    var self   = this;
+    self.props = {};
+
+    _.extend( self, {
+      $onChanges : $onChanges,
+      $onInit    : $onInit,
+      $postLink  : $postLink,
+      $onDestroy : $onDestroy,
+      tagAdded   : tagAdded,
+      tagAdding  : tagAdding,
+      tagRemoved : tagRemoved,
+      tagRemoving: tagRemoving,
+      invalidTag : invalidTag,
+      tagClicked : tagClicked
+    } );
+
+    ////////////
 
     // ****************************************
-    // LIFECYCLE HOOKS
+    // COMPONENT LIFECYCLE EVENTS
     // ****************************************
-    self.$onChanges = $onChanges;
-    self.$onInit    = $onInit;
-    self.$postLink  = $postLink;
-    self.$onDestroy = $onDestroy;
-
-    /////////////////////////
-
     function $onChanges( changes ) {
       log.info( '$onChanges' );
+      if ( changes.rule ) {
+        self.rule = angular.copy( self.rule );
+        if ( !changes.rule.isFirstChange() ) {
+          singleSelectRule();
+        }
+      }
     }
 
     function $onInit() {
-      log.info( '$onInit' );
+      initProps();
+      initRules();
     }
 
     function $postLink() {
-      log.info( '$postLink' );
-
-      self.tagAdded = function( tag ) {
-        log.debug( 'Tag Added: ', tag );
-        self.onAdd( { $event: { tags: self.tags } } );
-      };
-
-      self.tagRemoved = function( tag ) {
-        log.debug( 'Tag Removed: ', tag );
-        self.onRemove( { $event: { tags: self.tags } } );
-      };
     }
 
     function $onDestroy() {
-      log.info( '$onDestroy' );
+    }
+
+    // ****************************************
+    // NG-TAGS-INPUT FUNCTIONS
+    // ****************************************
+    function tagAdded( $tag ) {
+      initRules();
+      self.onAdd( { $event: { tags: self.tags } } );
+    };
+
+    function tagAdding( $tag ) {
+      self.onAdding( { $event: { tag: $tag } } );
+    };
+
+    function tagRemoved( $tag ) {
+      initRules();
+      self.onRemove( { $event: { tags: self.tags } } );
+    };
+
+    function tagRemoving( $tag ) {
+      self.onRemoving( { $event: { tag: $tag } } );
+    };
+
+    function invalidTag( $tag ) {
+      self.onInvalid( { $event: { tag: $tag } } );
+    };
+
+    function tagClicked( $tag ) {
+      self.onClicked( { $event: { tag: $tag } } );
+    };
+
+    // ****************************************
+    // COMPONENT FUNCTIONS
+    // ****************************************
+    function initProps() {
+      setComponentProperties();
+    }
+
+    function initRules() {
+      singleSelectRule();
+    }
+
+    function setComponentProperties() {
+
+      var formConfig    = lookaheadConfiguration.form;
+      var tagsInputs    = lookaheadConfiguration.tagsInputs;
+      var autoCompletes = lookaheadConfiguration.autoComplete;
+
+      log.debug( 'formConfig', formConfig );
+      log.debug( 'tagsInputs', tagsInputs );
+      log.debug( 'autoComplete', autoCompletes );
+
+      var props = { form: {}, tagsInput: {}, autoComplete: {} };
+
+      _.forIn( formConfig, function( value, key ) {
+        props.form[key] = value;
+      } );
+
+      _.forIn( tagsInputs, function( value, key ) {
+        props.tagsInput[key] = value;
+      } );
+
+      _.forIn( autoCompletes, function( value, key ) {
+        props.autoComplete[key] = value;
+      } );
+
+      self.props = props;
+      log.debug( self.props );
+    }
+
+    // Rule checks to see if only one tag selection is allowed in component
+    function singleSelectRule() {
+      self.props.autoComplete.maxResultsToShow = 10;
+      self.props.tagsInput.placeholder         = 'User:';
+
+      if ( self.rule.singleSelectMode && self.tags.length === 1 ) {
+        self.props.autoComplete.maxResultsToShow = 0;
+        self.props.tagsInput.placeholder         = '(SINGLE SELECT MODE)';
+      } else if ( self.rule.singleSelectMode && self.tags.length === 2 ) {
+        self.tags.splice( 1, 1 );
+        self.props.autoComplete.maxResultsToShow = 0;
+        self.props.tagsInput.placeholder         = '(SINGLE SELECT MODE)';
+      } else if ( self.rule.singleSelectMode && self.tags.length > 2 ) {
+        for ( var i = self.tags.length; i--; ) {
+          self.tags.splice( i, 1 );
+        }
+      }
     }
   }
 
--- src/components/foundation/base/modules/isc.lookahead/isc.lookahead.config.provider.js
+++ src/components/foundation/base/modules/isc.lookahead/isc.lookahead.config.provider.js
@@ -3,22 +3,87 @@
 
   angular
     .module( 'isc.lookahead' )
-    .provider( 'lookaheadConfig', lookaheadConfig );
+    .provider( 'lookaheadConfiguration', lookaheadConfiguration );
 
-  function lookaheadConfig() {
-    var self = this;
-    self.conf = {};
+  /**
+   *
+   * @returns {{defaults: *, setUserConfiguration: setUserConfiguration, $get: $get}}
+   */
+  function lookaheadConfiguration() {
+
+    var self                = this;
+    self.formConfig         = {};
+    self.tagsInputConfig    = {};
+    self.autoCompleteConfig = {};
+
+    self.defaults = {
+      "form"        : {
+        debug : false,
+        formId: "tags"
+      },
+      "tagsInput"   : {
+        required               : true,
+        template               : "tag-template",
+        displayProperty        : "text",
+        keyProperty            : "text",
+        tabindex               : 10,
+        placeholder            : "Add Tag:",
+        minLength              : 2,
+        maxLength              : 25,
+        minTags                : 1,
+        maxTags                : 25,
+        addOnEnter             : true,
+        addOnSpace             : false,
+        addOnComma             : false,
+        addOnBlur              : false,
+        addOnPaste             : false,
+        pasteSplitPattern      : ",",
+        replaceSpacesWithDashes: false,
+        enableEditingLastTag   : false,
+        addFromAutocompleteOnly: false
+      },
+      "autoComplete": {
+        template            : "autocomplete-template",
+        displayProperty     : "text",
+        debounceDelay       : 100,
+        minLength           : 2,
+        highlightMatchedText: true,
+        maxResultsToShow    : 10,
+        loadOnDownArrow     : false,
+        loadOnEmpty         : false,
+        loadOnFocus         : false,
+        selectFirstMatch    : true
+      }
+    };
+
+    ////////////
 
     return {
-      setFormConfig    : function( value ) {
-        self.conf.formConfig = value;
+      defaults            : self.defaults,
+      setUserConfiguration: function( user ) {
+        self.user = user;
       },
-      setTemplateConfig: function( value ) {
-        self.conf.templateConfig = value;
-      },
-      $get             : function() {
-        return self.conf;
+      $get                : function() {
+        angular.extend( self.formConfig, self.defaults.form, self.user.form );
+        angular.extend( self.tagsInputConfig, self.defaults.tagsInput, self.user.tagsInput );
+        angular.extend( self.autoCompleteConfig, self.defaults.autoComplete, self.user.autoComplete );
+
+        self.component = {
+          "form"        : self.formConfig,
+          "tagsInput"   : self.tagsInputConfig,
+          "autoComplete": self.autoCompleteConfig
+        };
+
+        return {
+          user        : self.user,
+          defaults    : self.defaults,
+          component   : self.component,
+          form        : self.formConfig,
+          tagsInputs  : self.tagsInputConfig,
+          autoComplete: self.autoCompleteConfig
+        };
       }
     };
   }
+
 } )();
