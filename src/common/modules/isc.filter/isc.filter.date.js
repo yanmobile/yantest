79cabc1e6119096b6c08dbf377f469b7384c2737
Elio Maggini <emaggini@usmbp15emaggini.iscinternal.com>
2016-12-08 12:52:56 -0500

    update of filter to use localized date formatting

--- src/common/modules/isc.filter/isc.filter.date.js
+++ src/common/modules/isc.filter/isc.filter.date.js
@@ -1,10 +1,10 @@
 /**
  * Created by Trevor Hudson on 10/27/15.
  */
-// This filter contains standards for Date display for consistency
-// for custom formatting, please refer to http://momentjs.com/docs/#/displaying/format/
-// First option takes in custom format strings, 'date', 'dateWithTime', or 'fromNow'
-// Second option is UTC: true indicates to use UTC mode
+  // This filter contains standards for Date display for consistency
+  // for custom formatting, please refer to http://momentjs.com/docs/#/displaying/format/
+  // First option takes in custom format strings, 'date', 'dateWithTime', or 'fromNow'
+  // Second option is UTC: true indicates to use UTC mode
 
 ( function() {
   'use strict';
@@ -12,7 +12,7 @@
   // injection
   // ----------------------------
   angular.module( 'isc.filters' )
-    .filter( 'iscDate', iscDate );
+      .filter( 'iscDate', iscDate );
 
   /* @ngInject */
   function iscDate( devlog ) {
@@ -20,6 +20,7 @@
     log.debug( 'iscDate LOADED' );
 
     return function( date, format, utc ) {
+
       if ( !moment ) {
         log.error( 'Error: momentJS is not loaded as a global' );
         return date;
@@ -33,13 +34,13 @@
       var iscMoment = utc ? moment.utc : moment;
 
       if ( !format ) { // September 26, 1986 9:00 AM
-        return iscMoment( date ).format( 'MMMM Do, YYYY h:mm A' );
+        return iscMoment( date ).format( 'LLL' );
       }
       if ( format === 'date' ) { // September 26, 1986
-        return iscMoment( date ).format( 'MMMM Do, YYYY' );
+        return iscMoment( date ).format( 'LL' );
       }
       else if ( format === 'dateWithTime' ) { // 9/26/1986 9:00 AM
-        return iscMoment( date ).format( 'M/D/YY h:mm A' );
+        return  iscMoment( date ).format( 'L' ) + ' ' + iscMoment( date ).format( 'LT' );
       }
       else if ( format === 'fromNow' ) { // 29 years ago
         return iscMoment( date ).fromNow();
