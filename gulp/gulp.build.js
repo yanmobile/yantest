1ef3634e52eb351a1f461ea7da97682017d2dffe
probbins <paul.robbins@intersystems.com>
2017-01-05 17:18:35 -0500

    Includes 'fdn' task in build and deploy scripts.

--- gulp/gulp.build.js
+++ gulp/gulp.build.js
@@ -9,7 +9,7 @@ module.exports = {
 
 function init(gulp, plugins, config, _) {
   gulp.task('build', ['clean', 'jshint'], function (done) {
-    var tasks = ['fonts', 'images', 'templates', 'scripts', 'mocks', 'sass', 'favicon', 'i18n', 'version'];
+    var tasks = ['fonts', 'images', 'fdn', 'templates', 'scripts', 'mocks', 'sass', 'favicon', 'i18n', 'version'];
     return plugins.seq(tasks, 'html', 'test', done);
   });
 }
