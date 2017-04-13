aea85970f067b65b2ef426fb69f9e020589ae5ca
probbins <paul.robbins@intersystems.com>
2017-04-13 13:39:17 -0400

    Adds logging to `iscAuthStatus` service and makes the `onSuccess` callback safer and more generic. Also makes the test for success in the /auth/status call configurable.

--- src/common/modules/isc.authentication/isc.authStatus.service.js
+++ src/common/modules/isc.authentication/isc.authStatus.service.js
@@ -9,9 +9,11 @@
     .factory( 'iscAuthStatusService', iscAuthStatusService );
 
   /* @ngInject */
-  function iscAuthStatusService( $rootScope, $window,
+  function iscAuthStatusService( devlog, $rootScope, $window,
     iscCookieManager, iscAuthStatus, iscSessionModel, iscSessionStorageHelper,
     NAV_EVENTS, AUTH_EVENTS ) {
+    var channel = devlog.channel( 'iscAuthStatus' );
+
     var thisTabHash,
         authStatusCallFailed = false;
 
@@ -23,16 +25,20 @@
     };
 
     function checkAuthStatus() {
+      channel.logFn( 'checkAuthStatus' );
+
       var config              = iscAuthStatus.getConfig(),
           storedLoginResponse = iscSessionStorageHelper.getLoginResponse(),
           isBrowserLoggedOut  = iscCookieManager.get( 'browserIsLoggedOut' );
 
+      channel.log( 'storedLoginResponse: ', storedLoginResponse );
       if ( config.authStatusUrl !== undefined &&
         _.isEmpty( storedLoginResponse ) && // this is checked in isc.authentication.run()
         !config.authStatusHasBeenChecked && !isBrowserLoggedOut ) {
 
         config.authStatusHasBeenChecked = true;
 
+        channel.log( 'calling authStatusUrl' );
         config.authResults = callAuthStatus( config.authStatusUrl );
 
         if ( config.authResults ) {
@@ -44,17 +50,21 @@
     }
 
     function authStatusFocus() {
+      channel.logFn( 'authStatusFocus' );
+
       var config       = iscAuthStatus.getConfig(),
           shouldLogOut = iscCookieManager.get( 'browserIsLoggedOut' ),
           callback     = config.authStatusFocusCallback || _.noop;
 
       if ( config.useAuthStatus ) {
         if ( shouldLogOut && iscSessionModel.isAuthenticated() ) {
+          channel.log( 'Not authenticated -- logging out' );
           $rootScope.$emit( AUTH_EVENTS.logout );
           return;
         }
 
         var storageHash = iscCookieManager.get( 'hashcode' );
+        channel.log( 'storageHash ', storageHash );
 
         if ( storageHash && storageHash !== thisTabHash ) {
           var authStatusResult = callAuthStatus( config.authStatusUrl );
@@ -67,6 +77,8 @@
     }
 
     function authStatusBlur() {
+      channel.logFn( 'authStatusBlur' );
+
       // If the last call to auth/status failed, do not update the local hash.
       // This will force auth/status to be retried when this window is focused again.
       if ( !authStatusCallFailed ) {
@@ -75,7 +87,10 @@
     }
 
     function callAuthStatus( url ) {
-      var myRet;
+      channel.logFn( 'callAuthStatus' );
+
+      var myRet,
+          config = iscAuthStatus.getConfig();
 
       //since this ajax call is synchronous, the return statement won't execute until this ajax call has returned and the callback executed
       $.ajax( {
@@ -89,13 +104,23 @@
       return myRet; //will be populated with onSuccess result if successful, else it'll remain as undefined
 
       function onSuccess( results ) {
-        if ( results.LoggedIn ) {
+        channel.log( '...onSuccess' );
+
+        // Some applications may have already parsed the result to JSON in an interceptor
+        results = _.isString( results ) ? JSON.parse( results ) : results;
+
+        // If no authStatusSuccessTest has been configured, or the configured one returns truthy,
+        // parse the results.
+        if ( !_.isFunction( config.authStatusSuccessTest ) || config.authStatusSuccessTest.call( null, results ) ) {
           myRet = results;
         }
+
         authStatusCallFailed = false;
       }
 
       function onError() {
+        channel.log( '...onError' );
+
         authStatusCallFailed = true;
       }
     }
